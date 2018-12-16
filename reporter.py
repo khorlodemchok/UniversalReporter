8a5c05fb599c92d59cdc7c022d271e239d34fa85
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2018-12-16 22:29:16 +0100

    some refactoring

--- reporter.py
+++ reporter.py
@@ -4,9 +4,11 @@ from collections.abc import Iterable, Iterator
 class Reporter(Iterable):
     def __init__(self):
         self.__key = None
+        self.__key_name = None
 
-    def set_key(self, k, name):
-        self.__key = iter(k)
+    def set_key(self, key, name):
+        self.__key = iter(key)
+        self.__key_name = name
 
     def __iter__(self):
         return self
