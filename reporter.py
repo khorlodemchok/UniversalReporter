d67793489c37dc81f9d6a56f608f3bf375e0a138
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2018-12-17 08:09:39 +0100

    one relation works

--- reporter.py
+++ reporter.py
@@ -5,10 +5,16 @@ class Reporter(Iterable):
     def __init__(self):
         self.__key = None
         self.__key_name = None
+        self.__rel = None
+        self.__rel_name = None
 
     def set_key(self, key, name):
         self.__key = iter(key)
         self.__key_name = name
+
+    def add_relation(self, func, name):
+        self.__rel = func
+        self.__rel_name = name
 
     def __iter__(self):
         return self
@@ -17,5 +23,8 @@ class Reporter(Iterable):
         if not self.__key:
             raise StopIteration
         result = dict()
-        result[self.__key_name] = next(self.__key)
+        key = next(self.__key)
+        result[self.__key_name] = key
+        if self.__rel:
+            result[self.__rel_name] = self.__rel(key)
         return result
--- test_reporter.py
+++ test_reporter.py
@@ -23,3 +23,58 @@ def test_iter():
     r = Reporter()
     r.set_key(items, "zupa")
     assert list(r) == expected
+
+
+def test_empty_relation():
+    def zero(_):
+        return None
+
+    items = ["spam"]
+    expected = [
+        {
+            "key": "spam",
+            "rel": None
+        }
+    ]
+
+    r = Reporter()
+    r.set_key(items, "key")
+    r.add_relation(zero, "rel")
+    assert list(r) == expected
+
+
+def test_one_relation():
+    reldata = {
+        "spam": "spam_val",
+        "eggs": "eggs_val",
+        "bacon": "bacon_val"
+    }
+
+    def data_func(k):
+        return reldata[k]
+
+    key = [
+        "spam",
+        "eggs",
+        "bacon"
+    ]
+
+    expected = [
+        {
+            "key": "spam",
+            "rel1": "spam_val"
+        },
+        {
+            "key": "eggs",
+            "rel1": "eggs_val"
+        },
+        {
+            "key": "bacon",
+            "rel1": "bacon_val"
+        }
+    ]
+
+    r = Reporter()
+    r.set_key(key, "key")
+    r.add_relation(data_func, "rel1")
+    assert list(r) == expected
