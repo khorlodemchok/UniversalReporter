2748eb7b9a8438f19944ea9d6fa214e37d6de7f9
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2018-12-19 08:36:34 +0100

    setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='UniversalReporter',
+      version='0.1',
+      description='Tool for gathering dta',
+      url='https://github.com/khorlodemchok/UniversalReporter',
+      author='Maciej Wawrzynczuk',
+      author_email='maciej.wawrzynczuk@gmail.com',
+      license='AGPL 3.0',
+      packages=['UniversalReporter'],
+      zip_safe=False)
